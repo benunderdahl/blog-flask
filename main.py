73d6ad9dc8b1b7a60a08d7ff2db37681d48fa4c7
benunderdahl <benjamin.underdahl@proton.me>
2025-09-28 09:54:59 -0500

    created add route for individual posts

--- main.py
+++ main.py
@@ -43,10 +43,11 @@ def get_all_posts():
     return render_template("index.html", all_posts=posts)
 
 # TODO: Add a route so that you can click on individual posts.
-@app.route('/')
+@app.route('/<int:post_id>')
 def show_post(post_id):
     # TODO: Retrieve a BlogPost from the database based on the post_id
-    requested_post = "Grab the post from your database"
+    requested_post = db.session.get(BlogPost, post_id)
+    print(requested_post)
     return render_template("post.html", post=requested_post)
 
 
